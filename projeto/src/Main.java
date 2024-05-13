f629cc91608bd73eadf73f1f0306a1bb252a783b
lewy <lewyalves78@gmail.com>
2024-05-13 09:30:27 -0300

    adicionando gerador de numeros aleatorios

--- projeto/src/Main.java
+++ projeto/src/Main.java
@@ -1,12 +1,18 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int n = 80;
+        Random gerador = new Random();
+        int n = gerador.nextInt(1000);
         Scanner adivinhador = new Scanner(System.in);
-        System.out.println("Bem vindo ao jogo de adivinhação!");
-        System.out.println("Acerte o número que estou pensando de 0 a 100");
+        String mensagem = """
+                Bem vindo ao jogo de adivinhação!
+                Acerte o número que estou pensando de 0 a 1000
+                Dica: Utilize pesquisa binaria para acertar de forma mais rapida!
+                """;
         var adivinha = 0;
+        System.out.println(mensagem);
 
         while (adivinha != n){
             adivinha = adivinhador.nextInt();
